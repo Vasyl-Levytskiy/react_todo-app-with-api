a0dd0dd13fb1c8b176b0f321a04a84a284b6ac07
Vasyl Levitskyi <leva1990vale@gmail.com>
2026-05-13 10:52:38 +0300

    add solution

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -60,21 +60,17 @@ export const TodoItem: React.FC<Props> = ({
         completed: todo.completed,
       })}
     >
-      <button
-        type="button"
-        className="todo__status-label"
-        onClick={() => onToggle(todo)}
-        disabled={isProcessing}
-        aria-label="Toggle todo status"
-      >
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label className="todo__status-label">
         <input
           data-cy="TodoStatus"
           type="checkbox"
           className="todo__status"
           checked={todo.completed}
-          readOnly
+          disabled={isProcessing}
+          onChange={() => onToggle(todo)}
         />
-      </button>
+      </label>
 
       {isEditing ? (
         <form onSubmit={handleSubmit}>
